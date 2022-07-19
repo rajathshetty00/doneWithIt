509c915d384aeec0027752ac5b57952b7196380b
Rajath Shetty <rajathshetty00@gmail.com>
2022-07-19 20:25:20 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
